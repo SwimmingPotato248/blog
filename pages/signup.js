99c5c6df500af8963025ff5fb1398d6f438fdf3f
SwimmingPotato <ptphong200i@gmail.com>
2022-09-20 09:25:59 +0700

    Add signup form with validation

--- pages/signup.js
+++ pages/signup.js
@@ -1,30 +1,52 @@
 import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
+const schema = z.object({
+  email: z.string().email(),
+  name: z.string(),
+  password: z
+    .string()
+    .min(5, { message: "Password must be at least 5 characters long" }),
+});
 
 export default function Signup() {
   const {
     register,
     handleSubmit,
-    watch,
     formState: { errors },
-  } = useForm();
+  } = useForm({ resolver: zodResolver(schema) });
 
   function onSubmit(data) {
     console.log(data);
   }
 
   return (
-    <>
+    <div className="flex flex-col justify-center items-center gap-5">
       <h1>Signup</h1>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div>
-          <label htmlFor="email">Email</label>
-          <input type="email" {...register("email", { required: true })} />
-        </div>
-        <div>
-          <label htmlFor="name">Display name</label>
-          <input type="text" {...register("name", { required: true })} />
-        </div>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col w-1/4 min-w-{300px} gap-3 "
+      >
+        <input type="email" {...register("email")} placeholder="Email" />
+        {errors.email?.message && <p>{errors.email?.message}</p>}
+
+        <input type="text" {...register("name")} placeholder="Display name" />
+        {errors.name?.message && <p>{errors.name?.message}</p>}
+
+        <input
+          type="password"
+          {...register("password")}
+          placeholder="Password"
+        />
+        {errors.password?.message && <p>{errors.password?.message}</p>}
+
+        <input
+          type="submit"
+          className="border-black border-1 rounded bg-blue-500 hover:bg-blue-700 py-2"
+          value="Register"
+        />
       </form>
-    </>
+    </div>
   );
 }
